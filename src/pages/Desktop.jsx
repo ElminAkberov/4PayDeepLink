19d8c43b99e0e01b8885da6a17717896acedd267
ElminAkberov <ekberovelmin04@gmail.com>
2025-05-21 13:36:54 +0400

    qrCode

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import qrCode from "../assets/img/qrcode.svg";
+import QRCode from "react-qr-code";
 const Desktop = () => {
+  const currentUrl = window.location.href;
   return (
     <section>
-      <div className="inter relative max-w-[350px] bg-white shadow-[0px_90px_200px_8px_#0000001A] rounded-[28px] ">
+      <div className="inter relative flex justify-center items-center w-[350px] aspect-square bg-white shadow-[0px_90px_200px_8px_#0000001A] rounded-[28px] ">
         <div className="bg-[#72E484] text-[14px] py-2 leading-5 text-center text-[#000000] mx-8 rounded-[12px] absolute top-[-30px]">
           Просканируйте QR код с мобильного устройства, чтобы перейти к оплате
         </div>
-        <img src={qrCode} alt="" className=" p-[30px]" />
+        <QRCode value={currentUrl} className=" p-[10px]" />
       </div>
     </section>
   );
