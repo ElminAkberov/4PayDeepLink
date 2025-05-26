8b53db0d12b8397d084f457d7eecad2e82b1c668
ElminAkberov <ekberovelmin04@gmail.com>
2025-05-26 16:38:28 +0400

    devLink

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://4pay.win/api/v1/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://dev.4pay.win/api/v1/" }),
   endpoints: (builder) => ({
     getDeeplink: builder.query({
       query: (uuid) => `deeplink/${uuid}`,
