824c9cf4eab5bcf298fca341c3cca52cf445bb6f
ElminAkberov <ekberovelmin04@gmail.com>
2025-05-28 13:34:55 +0400

    card_number

--- src/pages/Mobile.jsx
+++ src/pages/Mobile.jsx
@@ -9,57 +9,41 @@ const Mobile = () => {
   const { uuid } = useParams();
   const navigate = useNavigate();
   const [selectedBank, setSelectedBank] = React.useState("");
-
   const { data, error, isLoading } = useGetDeeplinkQuery(uuid, {
     skip: !uuid,
   });
-
   const templateBase64 =
     "dGlua29mZmJhbms6Ly9NYWluL1BheUJ5TW9iaWxlTnVtYmVyP251bWJlclBob25lPSZhbW91bnQ9JmJhbmtNZW1iZXJJZD0=";
-  const templateBase64_Ios =
-    "dC1iYW5rOi8vTWFpbi9QYXlCeU1vYmlsZU51bWJlcj9udW1iZXJQaG9uZT0mYW1vdW50PSZiYW5rTWVtYmVySWQ9";
-  const templateBase64_Ios_2 =
-    "dC1wb21vc2hjaDovL01haW4vUGF5QnlNb2JpbGVOdW1iZXI/bnVtYmVyUGhvbmU9JmFtb3VudD0mYmFua01lbWJlcklkPQ==";
-
-  const fillTemplate = (templateBase64, data) => {
-    const template = atob(templateBase64);
+  const cardTemplate64 =
+    "dGlua29mZmJhbms6Ly9NYWluL1BheS9DMkM/dGFyZ2V0Q2FyZE51bWJlcj0mYW1vdW50PQ==";
+  const template = atob(templateBase64);
+  const cardtemplate = atob(cardTemplate64);
+  
+  function fillTemplate(template, data) {
+    let filled;
     const baseUri = template.split("?")[0];
-    const numberPhone = data?.sbp_phone_number || "";
+    let numberPhone = data?.sbp_phone_number || "";
     const amount = data?.amount || "";
     const bankMemberId = data?.bank?.bik || "";
+    const card_number = data?.card_number || "";
 
-    return `${baseUri}?numberPhone=${numberPhone}&amount=${amount}${
-      bankMemberId ? `&bankMemberId=${bankMemberId}` : ""
-    }`;
-  };
-
-  const isIOS =
-    /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-
-  const handlePay = () => {
-    if (!data || error || selectedBank !== "tBank") return;
-
-    if (isIOS) {
-      const urls = [
-        fillTemplate(templateBase64_Ios, data),
-        fillTemplate(templateBase64_Ios_2, data),
-        fillTemplate(templateBase64, data),
-      ];
-
-      let delay = 0;
-      urls.forEach((url) => {
-        setTimeout(() => {
-          window.location.href = url;
-        }, delay);
-        delay += 2000;
-      });
+    if (card_number) {
+      filled = `${baseUri}?targetCardNumber=${card_number}&amount=${amount}`;
     } else {
-      const androidUrl = fillTemplate(templateBase64, data);
-      window.location.href = androidUrl;
+      filled = `${baseUri}?numberPhone=${numberPhone}&amount=${amount}${
+        bankMemberId && `&bankMemberId=${bankMemberId}`
+      }`;
     }
-  };
+    return filled;
+  }
+  const filledUri = fillTemplate(
+    data?.card_number ? cardtemplate : template,
+    data
+  );
 
-  const handleBankClick = (bank) => setSelectedBank(bank);
+  const handleBankClick = (bank) => {
+    setSelectedBank(bank);
+  };
 
   return (
     <div className="text-black flex flex-col gap-4 mb-10">
@@ -99,18 +83,21 @@ const Mobile = () => {
 
       <button
         disabled={!data || error}
-        onClick={handlePay}
+        onClick={() => {
+          if (selectedBank == "tBank") {
+            window.location.href = filledUri;
+          }
+        }}
         className="bg-[#72E484] active:opacity-85 disabled:bg-[#ccc] disabled:pointer-events-none font-[500] py-2 rounded-[24px] text-sm mt-4"
       >
         Оплатить
       </button>
-
       <button
-        onClick={() =>
+        onClick={() => {
           navigate("/info", {
             state: { data, loading: isLoading, uuid },
-          })
-        }
+          });
+        }}
         className="border border-black font-[500] py-2 rounded-[24px] text-sm"
       >
         Другой банк
